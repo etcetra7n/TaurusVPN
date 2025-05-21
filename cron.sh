2f69b0f94e3108d29df7f1ad51e77189344a584a
John Anchery <etcetra7n@gmail.com>
2025-05-21 09:52:09 +0530

    change to dynu.com

--- cron.sh
+++ cron.sh
@@ -7,14 +7,14 @@ if ! sudo wg show | grep -q "latest handshake"; then # Latest handshake is not f
 		(
 			cat <<-EOF
 			Subject: TaurusVPN: Shutting down because no connection was recieved
-			To: {TO EMAIL}@gmail.com
+			To: { TO_EMAIL }@gmail.com
 			From: comm-system-noreply@taurusvpn.com
 			The server is shutting down because no client was connected within 4 minutes of running. This is an automatic email from the TaurusVPN E2.micro comm system
 
 			EOF
 			echo Uptime: $uptime_min min $uptime_sec sec
 			echo Timestamp: $(TZ="Asia/Kolkata" date +"%Y-%M-%d %H:%M:%S") IST
-		) | msmtp {TO EMAIL}@gmail.com
+		) | msmtp { TO_EMAIL }@gmail.com
 		sudo /sbin/shutdown now
 	fi
 	exit 0
@@ -27,7 +27,7 @@ if	[ "$last_hs_min" -ge 4 ]; then
 	(
 		cat <<-EOF
 		Subject: TaurusVPN: Shutting down because no active clients are remaining
-		To: {TO EMAIL}@gmail.com
+		To: { TO_EMAIL }@gmail.com
 		From: comm-system-noreply@taurusvpn.com
 		Your server is shutting down because 4 minutes have passed since last handshake with a client. This is an automatic email from the TaurusVPN E2.micro comm system
 
@@ -39,6 +39,6 @@ if	[ "$last_hs_min" -ge 4 ]; then
 		echo Server IP: $(curl -s ifconfig.me)
 		echo Cleint IP: $(sudo wg show | grep 'endpoint' | awk -F'[:]+' '{print $2}')
 		echo Last handshake: $(sudo wg show | grep 'latest handshake' | awk -F'[:]+' '{print $2}')
-	) | msmtp {TO EMAIL}@gmail.com
+	) | msmtp { TO_EMAIL }@gmail.com
 	sudo /sbin/shutdown now
 fi
--- startup.sh
+++ startup.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-res=$(curl -s -o /dev/null -w "%{http_code}" --user {DOMAIN}:{PRIVATE KEY} https://update.dedyn.io/)
+myip=$(curl -s ifconfig.me)
+res=$(curl -s -o /dev/null -w "%{http_code}" -X GET "https://api.dynu.com/nic/update?myip=$myip" -H "Authorization: Basic { BASE64_ENCODED_USERNAME:PASSWORD }")
 
 if [ "$res" = "200" ]; then
 	(
 		echo Subject: TaurusVPN: Started at $(TZ="Asia/Kolkata" date +"%H:%M")
 		cat <<-EOF
-		To: {TO EMAIL}@gmail.com
+		To: { TO_EMAIL }@gmail.com
 		From: comm-system-noreply@taurusvpn.com
 		This is an automatic email from the E2.micro comm system, triggered when the server starts up. I'm here to tell you that TaurusVPN is up and ready for connection
 
 		EOF
-		echo Server IP: $(curl -s ifconfig.me)
+		echo Server IP: $myip
 		echo Timestamp: $(TZ="Asia/Kolkata" date +"%Y-%M-%d %H:%M:%S") IST
-	) | msmtp {TO EMAIL}@gmail.com
+	) | msmtp { TO_EMAIL }@gmail.com
 else
 	(
 		echo Subject: TaurusVPN: Unable to update dynamic IP address
 		cat <<-EOF
-		To: {TO EMAIL}@gmail.com
+		To: { TO_EMAIL }@gmail.com
 		From: comm-system-noreply@taurusvpn.com
 		The desec.io api did not return a positive response to a update ip request. Your server will continue to listen for connection for 4 minutes but you might have to manually update the server IP on the client side. This is an automatic email from the TaurusVPN E2.micro comm system, triggered when the server starts up
 
@@ -26,5 +27,5 @@ else
 		echo Desec.io response: $(res)
 		echo Server IP: $(curl -s ifconfig.me)
 		echo Timestamp: $(TZ="Asia/Kolkata" date +"%Y-%M-%d %H:%M:%S") IST
-	) | msmtp {TO EMAIL}@gmail.com
+	) | msmtp { TO_EMAIL }@gmail.com
 fi
